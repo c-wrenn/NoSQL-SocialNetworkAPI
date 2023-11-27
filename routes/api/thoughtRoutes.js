cd966eeb35b4d2cf7bf4082ab2c9a23b6a9e2afb
c-wrenn <crislynwren@yahoo.com>
2023-11-27 02:18:53 -0600

    "thoughtRoutes complete"

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,13 +1,28 @@
 const router = require('express').Router();
 const {
-getThoughts,
-getSingleThought,
-createnewThoughts,
+getThoughts, //get
+createnewThoughts, //post
+getSingleThought, //get
+deleteThought, //delete
+createReaction, //post
+removeReaction,//delete
+updateThought, //optional ,put
+
+
+
+
 
 
 } = require('../../controllers/thoughtController');
 
+//GET to get all thoughts & POST to create a new thought
+// (don't forget to push the created thought's _id to the associated user's thoughts array field)
+router.route("/").get(getThoughts).post(createnewThoughts);
+//GET to get a single thought by its _id, and DELETE thought by its id
+router.route('/:thoughtId').get(getSingleThought).delete(deleteThought);
+//POST to create a reaction stored in a single thought's reactions array field
+router.route('/:thoughtId/reactions').post(createReaction);
+//DELETE to pull and remove a reaction by the reaction's reactionId value
+router.route("/:thoughtId/reactions/:reactionId").delete(removeReaction);
 
-router.route("/api/thoughts").get(getThoughts).get(getSingleThought)
-
-router.route("/api/thoughts").post(createnewThoughts)
+module.exports = router;
