5337e8e2e99cb4aff941d33eeecdac22f788de18
c-wrenn <crislynwren@yahoo.com>
2023-11-27 16:22:06 -0600

    "connection fixed"

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,12 @@
-const { connect, connection } = require('mongoose');
+// const { connect, connection } = require('mongoose');
 
-// Node will look for this environment variable and if it exists, it will use it. Otherwise, it will assume that this application is running locally
-const connection =
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/nosql-socialnetworkapi'
+// // Node will look for this environment variable and if it exists, it will use it. Otherwise, it will assume that this application is running locally
+// const connection =
+//   process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/nosql-socialnetworkapi'
 
-connect(connection);
+// connect(connection);
+const mongoose = require('mongoose')
 
-module.exports = connection;
+mongoose.connect('mongodb://127.0.0.1:27017/nosql-socialnetworkapi')
+
+module.exports = mongoose.connection;
